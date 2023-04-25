894c8da1669d2006f152c1c9f27ecaa4c1d826fe
Eduard Luhtonen <luhtonen@gmail.com>
2023-04-25 17:50:01 +0200

    define Block structure

--- src/ed_crystal_coin/block.cr
+++ src/ed_crystal_coin/block.cr
@@ -1,15 +1,21 @@
 require "openssl"
 
 class EdCrystalCoin::Block
-  def initialize(data : String)
+  property current_hash : String
+
+  def initialize(index = 0, data = "data", previous_hash = "hash")
     @data = data
+    @index = index
+    @timestamp = Time.utc
+    @previous_hash = previous_hash
+    @current_hash = hash_block
   end
 
-  def hash
+  private def hash_block
     hash = OpenSSL::Digest.new("SHA256")
-    hash.update(@data)
+    hash.update("#{@index}#{@timestamp}#{@data}#{@previous_hash}")
     hash.hexfinal
   end
 end
 
-puts EdCrystalCoin::Block.new("Hello, Cryptos!").hash
+puts EdCrystalCoin::Block.new(data: "Same Data").current_hash
